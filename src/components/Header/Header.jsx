731a26fe7447730f4990d8ca327a7df4821d34c2
abhiiishek07 <abhishek.209202156@muj.manipal.edu>
2024-03-05 15:50:58 +0530

    Work on homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [crossBar, setCrossBar] = useState(false);
   return (
-    <div className="navbar  shadow-md border rounded-md">
+    <div className="navbar py-3 rounded-md sticky top-0 bg-base-100 z-10">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">real-estate</a>
+        <Link className="btn btn-ghost normal-case text-3xl font-bold" href="/">
+          Real Estate
+        </Link>
       </div>
-      <div className="flex-none">
+
+      <div className=" font-medium hidden md:flex">
         <ul className="menu menu-horizontal px-1 uppercase">
           <li>
             <a>Home</a>
@@ -51,6 +57,60 @@ const Header = () => {
           </li>
         </ul>
       </div>
+      <div
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        onClick={() => setCrossBar(!crossBar)}
+      >
+        {crossBar ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {crossBar && (
+        <div className="flex flex-col  absolute top-0 left-0 w-full h-screen bg-white bg-gradient-to-br from-base-200 to-bas">
+          <ul className="menu menu-vertical px-1 uppercase mt-36 text-lg font-bold">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About us</a>
+            </li>
+            <li>
+              <details>
+                <summary>Properties</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Commercial</a>
+                  </li>
+                  <li>
+                    <a>Residental</a>
+                  </li>
+                  <li>
+                    <a>Sco</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Resources</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Blogs</a>
+                  </li>
+                  <li>
+                    <a>Location</a>
+                  </li>
+                  <li>
+                    <a>Social Connect</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>contact us</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/constants/constants.js
@@ -0,0 +1,40 @@
+export const A_PLUS_DEVELOPERS = [
+  "Smart World",
+  "M3M",
+  "EMAAr India",
+  "DLF",
+  "Godrej",
+  "Whiteland",
+  "Omaxe",
+];
+
+export const SLIDER_SETTINGS = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 2000,
+  autoplaySpeed: 2000,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+  ],
+};
