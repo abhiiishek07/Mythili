7372a3ac0ed812a5d4f29d739a6bd365636706ed
abhiiishek07 <abhishek.209202156@muj.manipal.edu>
2024-03-10 23:44:16 +0530

    vercel fixes

---
+++ src/pages/commercial-page/index.js
@@ -0,0 +1,8 @@
+import Commercial from "@/screens/Properties/Commercial";
+import React from "react";
+
+const CommercialPage = () => {
+  return <Commercial />;
+};
+
+export default CommercialPage;
