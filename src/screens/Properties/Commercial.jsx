fe8f0854f9b125a73c4d7b3eb3230c99ed553b3e
abhiiishek07 <abhishek.209202156@muj.manipal.edu>
2024-03-10 23:38:31 +0530

    fix issue

--- src/screens/Properties/Commercial.jsx
+++ src/screens/Properties/Commercial.jsx
@@ -6,10 +6,10 @@ const Commercial = () => {
     <div className="min-h-screen w-full flex flex-col overflow-hidden px-6 md:px-10 py-2 my-6">
       {/* Heading section */}
       <div className=" items-start justify-start mx-9 rounded-md max-w-md py-2">
-          <p className="text-2xl font-bold ">Commercial Property</p>
-          <p className="text-sm text-gray-600">
-            Explore our range of commercial properties for your business needs.
-          </p>
+        <p className="text-2xl font-bold ">Commercial Property</p>
+        <p className="text-sm text-gray-600">
+          Explore our range of commercial properties for your business needs.
+        </p>
       </div>
       <span className="w-full flex border border-gray-300 my-2 mx-8"></span>
       {/* property card division */}
