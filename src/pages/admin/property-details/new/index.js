222d40601721fd92cd3e9fb749242940690e734e
Rakshit <rakshitagarwalrocks@gmail.com>
2024-04-21 23:08:44 +0530

    added the new for Property-detail

---
+++ src/pages/admin/property-details/new/index.js
@@ -0,0 +1,8 @@
+import NewProperty from "@/screens/NewProperty/NewProperty";
+import React from "react";
+
+const PropertyDetailNew = () => {
+  return <NewProperty/>;
+};
+
+export default PropertyDetailNew;
